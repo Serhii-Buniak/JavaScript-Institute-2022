422e94b8e466efce840d380617a0fa84b1f1b954
Serhii Buniak <buniak.serhii@gmail.com>
2022-09-28 19:57:45 +0300

    lab3

--- lab3/index.js
+++ lab3/index.js
@@ -1,10 +1,57 @@
 function str_del() {
     selection = window.getSelection().toString()
-    let str = document.getElementById("elem").textContent
+    let str = document.getElementById("elem1").textContent
 
     let start = str.indexOf(selection)
     let end = start + selection.length
     let result = str.slice(0, start) + str.slice(end)
 
-    document.getElementById("elem").textContent = result
+    document.getElementById("elem1").textContent = result
+}
+
+function str_del_sym() {
+    let str = document.getElementById("elem2").textContent
+    let result = str.slice(0, str.length - 1)
+
+    document.getElementById("elem2").textContent = result
+}
+
+function getProperty(obj, prop) {
+    return obj[prop]
+}
+
+function addProperty(obj, prop, value) {
+    obj[prop] = value
+}
+
+function removeProperty(obj, prop) {
+    delete obj[prop]
+}
+
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName
+}
+
+function getLengthOfTwoWords(word) {
+    return word.length
+}
+
+function isGreaterThan(first, second) {
+    return first < second
+}
+
+function isEven(number) {
+    return number => number % 2 === 0
+}
+
+function isSameLength(first, second) {
+    return first.length === second.length
+}
+
+function isEvenAndGreaterThanTen(number) {
+    return isEven(number) && isGreaterThan(10, number)
+}
+
+function computeAreaOfATriangle(height, basis) {
+    return height * basis / 2
 }
